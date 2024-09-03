3f0089fc7faa3671059653ee34c4558aa3a0e871
Khaled Mahmoud <122036315+KhaledMa7mouad@users.noreply.github.com>
2024-09-03 16:56:23 +0300

    Note a Note

---
+++ app/src/main/java/com/example/notesapp/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.notesapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import com.example.notesapp.routes.AppNavHost
+import com.example.notesapp.ui.theme.NotesAppTheme
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            NotesAppTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { _ ->
+                    AppNavHost()
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/notesapp/database/Note.kt
@@ -0,0 +1,16 @@
+package com.example.notesapp.database
+
+import android.os.Parcelable
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+@Entity("note")
+class Note(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    @ColumnInfo("note_details")
+    val noteDetails: String
+) : Parcelable
